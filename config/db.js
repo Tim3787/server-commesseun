724bb130f0085e48c8aa669d9d790721e1f7cc78
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-01-14 18:07:48 +0100

    Primo tentativo amz

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
   host: "database-1.ct2yk8ewuzpb.eu-north-1.rds.amazonaws.com",
   user: "commesseun",
   password: "66548765443",
-  database: "commesseun3",
+  database: "gestione_commesse",
   port: 3306,
   timezone: "Z", // Forza UTC
 });

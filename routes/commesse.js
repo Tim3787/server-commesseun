8a7ef3b4286a950ca4d7c06470aed88c9a5ce2d2
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-01-20 13:37:50 +0100

    stato commesse 2

--- routes/commesse.js
+++ routes/commesse.js
@@ -147,7 +147,7 @@ router.put("/:id", async (req, res) => {
 
     // Esegui l'aggiornamento della commessa, mantenendo invariato il campo 'stati_avanzamento'
     await db.query(
-      `UPDATE commesse SET numero_commessa = ?, tipo_macchina = ?, descrizione = ?, data_consegna = ?,data_FAT = ?, altri_particolari = ?, cliente = ?, stato = ? WHERE id = ?`,
+      `UPDATE commesse SET numero_commessa = ?, tipo_macchina = ?, descrizione = ?, data_consegna = ?, data_FAT = ?, altri_particolari = ?, cliente = ?, stato = ? WHERE id = ?`,
       [
         numero_commessa,
         tipo_macchina,
@@ -156,8 +156,8 @@ router.put("/:id", async (req, res) => {
         data_FAT,
         altri_particolari,
         cliente,
-        id,
-        stato,
+        stato, // Corretto: stato prima di id
+        id,    // id va come ultimo elemento
       ]
     );
 

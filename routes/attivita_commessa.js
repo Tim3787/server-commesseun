7d459c96098cedcffce545e838ebdacaffe195ec
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-02-11 07:49:03 +0100

    test notifiche 2

--- routes/attivita_commessa.js
+++ routes/attivita_commessa.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
-
+const jwt = require("jsonwebtoken");
 
 // Middleware per ottenere l'id utente dal token
 const getUserIdFromToken = (req, res, next) => {

ddae7ac3eafa9bf12ac897ddc47b48b5b887a1ab
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-06-08 12:39:19 +0200

    datacreazione

--- routes/schedeTecniche.js
+++ routes/schedeTecniche.js
@@ -34,23 +34,16 @@ router.get("/:commessaId/schede", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const [results] = await db.query(`
-SELECT 
-  s.id, 
-  s.commessa_id, 
-  c.numero_commessa,   
-  t.nome AS tipo,   
-  s.titolo, 
-  s.intestazione, 
-  s.contenuto, 
-  s.note, 
-  s.data_modifica,
-  s.data_creazione,
-  u.nome AS creato_da_nome
-FROM SchedeTecniche s
-JOIN TipiSchedaTecnica t ON s.tipo_id = t.id
-JOIN commesse c ON s.commessa_id = c.id
-LEFT JOIN users u ON s.creata_da = u.id
-ORDER BY s.data_modifica DESC;
+      SELECT 
+        s.id, s.commessa_id, s.tipo_id, s.titolo,
+        s.intestazione, s.contenuto, s.note,
+        s.data_modifica, s.data_creazione, s.creata_da,
+        t.nome AS tipo,
+        u.username AS creato_da_nome
+      FROM SchedeTecniche s
+      JOIN TipiSchedaTecnica t ON s.tipo_id = t.id
+      LEFT JOIN users u ON s.creata_da = u.id
+      ORDER BY s.data_modifica DESC,
     `);
     res.json(results);
   } catch (err) {

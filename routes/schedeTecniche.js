0516fc017d675635489e2002114f4f6f11017712
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-06-08 16:12:26 +0200

    upload im6

--- routes/schedeTecniche.js
+++ routes/schedeTecniche.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
+const path = require("path");
 
 
 // 🔹 GET tutte le schede per una commessa

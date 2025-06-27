a4ba9fc2fe719e0cd8032a794191fa1a71dd36fa
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-06-27 16:06:04 +0200

    Update commesse

--- routes/attivita_commessa.js
+++ routes/attivita_commessa.js
@@ -274,25 +274,24 @@ router.put("/:id", getUserIdFromToken, async (req, res) => {
    // Recupera l'attività aggiornata con join per reparto e risorsa
 const [attivitaAggiornata] = await db.query(`
   SELECT 
-    a.id,
-    a.commessa_id,
-    a.risorsa_id,
-    u.nome AS nome_risorsa,
-    a.attivita_id,
-    at.nome_attivita,
-    a.data_inizio,
-    a.durata,
-    a.descrizione,
-    a.stato,
-    a.included_weekends,
-    r.id AS reparto_id,
-    r.nome AS nome_reparto
-  FROM attivita_commessa a
-  JOIN attivita at ON at.id = a.attivita_id
-  JOIN risorse ri ON ri.id = a.risorsa_id
-  JOIN reparti r ON r.id = ri.reparto_id
-  JOIN users u ON u.risorsa_id = ri.id
-  WHERE a.id = ?
+  a.id,
+  a.commessa_id,
+  a.risorsa_id,
+  ri.nome AS nome_risorsa,
+  a.attivita_id,
+  at.nome_attivita,
+  a.data_inizio,
+  a.durata,
+  a.descrizione,
+  a.stato,
+  a.included_weekends,
+  r.id AS reparto_id,
+  r.nome AS nome_reparto
+FROM attivita_commessa a
+JOIN attivita at ON at.id = a.attivita_id
+JOIN risorse ri ON ri.id = a.risorsa_id
+JOIN reparti r ON r.id = ri.reparto_id
+WHERE a.id = ?
 `, [id]);
 
 // Invia l’attività aggiornata al frontend

3b6d29276fc9033f77cd18e862ccc6cc24ef52d1
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2025-11-21 16:04:44 +0100

    stato commessa put

--- routes/commesse.js
+++ routes/commesse.js
@@ -129,7 +129,7 @@ router.put("/:id", async (req, res) => {
     data_FAT = null,
     altri_particolari = "",
     cliente = "",
-     stato_commessa,
+    stato,
   } = req.body;
 
   try {
@@ -157,7 +157,7 @@ router.put("/:id", async (req, res) => {
         data_FAT,
         altri_particolari,
         cliente,
-         stato_commessa, // Corretto: stato prima di id
+        stato, // Corretto: stato prima di id
         id,    // id va come ultimo elemento
       ]
     );

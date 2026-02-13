4ae44c1d3f9bbdf912e8723cba03ea76d80c7456
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2026-02-13 16:57:43 +0100

    upload

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 const tagsRoutes = require('./routes/tags');
+const uploadRoute = require('./routes/upload');
 
 // Middleware di sicurezza e configurazione
 app.use(helmet()); // Aggiunge intestazioni di sicurezza
